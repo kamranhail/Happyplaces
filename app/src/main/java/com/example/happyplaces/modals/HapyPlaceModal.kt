dad26d532136b494d5e8f5daa33fccd79fa51979
muhammad  kamran yousaf <kamranhail08@gmail.com>
2022-12-04 01:04:36 +0100

    second commit

---
+++ app/src/main/java/com/example/happyplaces/modals/HapyPlaceModal.kt
@@ -0,0 +1,14 @@
+package com.example.happyplaces.modals
+
+data class HapyPlaceModal(
+    val id : Int,
+    val title :String,
+    val image :String,
+    val description : String,
+    val data : String,
+    val location : String,
+    val latitdue: Double,
+    val longitude : Double
+)
+
+
